5d9622818696d10fcda9887d329f28f1a00b87a2
Joshua Lopez <44761768+jflopezr11@users.noreply.github.com>
2020-03-18 09:23:59 -0700

    first commit

---
+++ NodeJSPractice/firstexercise/play.js
@@ -0,0 +1,64 @@
+// var name = 'Josh';
+
+// console.log(name);
+
+// var name = 'Max';
+// var age = 29;
+// var hasHobbies = true;
+
+// function summarizeUser(userName, userAge, userHasHobby) {
+//   return (
+//     'Name is ' +
+//     userName +
+//     ', age is ' +
+//     userAge +
+//     ' and the user has hobbies: ' +
+//     userHasHobby
+//   );
+// }
+
+// console.log(summarizeUser(name, age, hasHobbies));
+
+
+// const name = 'Max';
+// let age = 29;
+// const hasHobbies = true;
+
+// age = 30;
+
+// const summarizeUser = (userName, userAge, userHasHobby) => {
+//   return (
+//     'Name is ' +
+//     userName +
+//     ', age is ' +
+//     userAge +
+//     ' and the user has hobbies: ' +
+//     userHasHobby
+//   );
+// };
+
+// // const add = (a, b) => a + b;
+// // const addOne = a => a + 1;
+// const addRandom = () => 1 + 2;
+
+// // console.log(add(1, 2));
+// // console.log(addOne(1));
+// console.log(addRandom());
+
+// console.log(summarizeUser(name, age, hasHobbies));
+
+
+// const person = {
+//     name: 'Max',
+//     age: 29,
+//     greet() {
+//       console.log('Hi, I am ' + this.name);
+//     }
+//   };
+  
+//   const hobbies = ['Sports', 'Cooking'];
+//   // for (let hobby of hobbies) {
+//   //     console.log(hobby);
+//   // }
+//   console.log(hobbies.map(hobby => 'Hobby: ' + hobby));
+//   console.log(hobbies);
