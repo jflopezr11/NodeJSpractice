94b6631ecd335c34064a01a645dd68850b3a4d19
Joshua Lopez-Roque <joshualopezroque@gmail.com>
2020-04-10 20:12:45 -0700

    using and learning 'express.router'

--- NodeJSPractice/thirdexercise/server.js
+++ NodeJSPractice/thirdexercise/server.js
@@ -1,14 +1,41 @@
-const http = require('http');
-
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+    res.status(404).send('<h1>Page Not Found</h1>')
+});
+
+app.listen(3000);
+
+/*
+app.use('/', (req, res, next) => {
+    // console.log('in another middleware');
+    res.send('<h1>Hello from Express!</h1>')
+});
+
+const server = http.createServer(app);
+*/
+
+
 
 // app.use((req, res, next) => {
 //     console.log('in the middleware');
 //     next(); // allows the 'app.use' below to run
 // });
 
+/*
 app.use('/', (req, res, next) => {
     console.log('This always runs!')
     next();
@@ -23,7 +50,4 @@ app.use('/', (req, res, next) => {
     console.log('in another middleware');
     res.send('<h1>Hello from Express!</h1>')
 });
-
-const server = http.createServer(app);
-
-server.listen(3000);
+*/
